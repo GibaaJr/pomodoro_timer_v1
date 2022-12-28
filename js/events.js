d094bb0b5b4d18430c47d659c93b6a9d61f3a0df
Gibaa <gilberto.alves.jr@gmail.com>
2022-12-28 13:00:22 -0300

    refactory

---
+++ js/events.js
@@ -0,0 +1,47 @@
+
+import {  buttonPlay, buttonPause, buttonSet, buttonStop, buttonSoundOff, buttonSoundOn } from "./elements.js"
+
+export default function events({ controls, timer, sound }) {
+        buttonPlay.addEventListener('click', () => {
+        controls.play()
+        timer.countdown()
+        sound.pressButton()
+    }),
+
+    buttonPause.addEventListener('click', () => {
+        controls.pause()
+        timer.hold()
+        sound.pressButton()
+    }),
+
+    buttonStop.addEventListener('click', () => {
+        controls.reset()
+        timer.reset()
+        sound.pressButton()
+    }),
+
+    buttonSoundOff.addEventListener('click', () => {
+        buttonSoundOn.classList.remove('hide')
+        buttonSoundOff.classList.add('hide')
+        sound.bgAudio.pause()
+    }),
+
+    buttonSoundOn.addEventListener('click', () => {
+        buttonSoundOff.classList.remove('hide')
+        buttonSoundOn.classList.add('hide')
+        sound.bgAudio.play()
+    }),
+
+    buttonSet.addEventListener('click', () => {
+    let newMinutes = controls.getMinutes()
+
+    if (!newMinutes) {
+        timer.reset()
+        return
+    }
+
+    let minutes = newMinutes
+    timer.updateDisplay(minutes, 0)
+    timer.updateMinutes(minutes)
+    })
+}
--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 import { Controls } from "./controls.js"
 import { Timer } from "./timer.js"
 import Sound from "./sounds.js"
+import Events from "./events.js"
 import { buttonPause, buttonPlay, buttonSet, buttonStop, buttonSoundOff, 
     buttonSoundOn, minutesDisplay, secondsDisplay } from "./elements.js"
-
-
-
 
 let minutes = Number(minutesDisplay.textContent)
 
@@ -25,47 +23,4 @@ const timer = Timer({
 
 const sound = Sound()
 
-
-// events
-buttonPlay.addEventListener('click', () => {
-    controls.play()
-    timer.countdown()
-    sound.pressButton()
-})
-
-buttonPause.addEventListener('click', () => {
-    controls.pause()
-    timer.hold()
-    sound.pressButton()
-})
-
-buttonStop.addEventListener('click', () => {
-    controls.reset()
-    timer.reset()
-    sound.pressButton()
-})
-
-buttonSoundOff.addEventListener('click', () => {
-    buttonSoundOn.classList.remove('hide')
-    buttonSoundOff.classList.add('hide')
-    sound.bgAudio.pause()
-})
-
-buttonSoundOn.addEventListener('click', () => {
-    buttonSoundOff.classList.remove('hide')
-    buttonSoundOn.classList.add('hide')
-    sound.bgAudio.play()
-})
-
-buttonSet.addEventListener('click', () => {
-   let newMinutes = controls.getMinutes()
-
-   if (!newMinutes) {
-    timer.reset()
-    return
-   }
-
-   minutes = newMinutes
-   timer.updateDisplay(minutes, 0)
-   timer.updateMinutes(minutes)
-})
+Events({controls, timer, sound})
